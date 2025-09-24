2d5de5a6f0112a4ff5f17abd59ebfac8b94d7982
pakdamie <damiedpak@gmail.com>
2025-09-23 20:39:17 -0400

    Fix: Updated the wrangle code to fully function

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # hotdog_regression
+
+What are the hotdog prices across the baseball stadiums? 
+Can we use regression to understand the facts associated wtih hotdog prices? 
+
+## Scraping information
+I scrape baseball stadium data from Wikipedia and scrape the hotdog prices from the Washington Journal of Sports (see scrape_data.py)
+
+
+
--- src/wrangle_data.py
+++ src/wrangle_data.py
@@ -1,22 +1,42 @@
 import pandas as pd
+from datetime import datetime
 
 
 def clean_stadium_data(stadium_data):
 
+    """
+    Wrangles the stadium_data that was scraped from wikipedia
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the Stadium name, capacity, team that it hosts, when it opened,
+        the age, the city, and the state
+    """
+
     #Removing unnecessary columns such as Image, Surface, Distance to center field, Type, Roof type
-    stadium_data = stadium_data.drop(['Image', 'Surface', "Distance to center field", "Type", "Roof type"], axis=1)
+    stadium_data = stadium_data.drop(['Image', 'Surface', 'Distance to center field', 'Type', 'Roof type'], axis=1)
     
-    #Remove the hyperlinks, remove the comma, and make it numeric
+    #Remove the hyperlinks, remove the comma, and make it numeric - Capacity
     stadium_data['Capacity'] = stadium_data['Capacity'].str.replace(r'\[\d+\]', '', regex=True)
-    stadium_data["Capacity"] = stadium_data["Capacity"].str.replace(",", "")
-    stadium_data["Capacity"] = pd.to_numeric(stadium_data["Capacity"])
+    stadium_data['Capacity'] = stadium_data['Capacity'].str.replace(',', '')
+    stadium_data['Capacity'] = pd.to_numeric(stadium_data['Capacity'])
+
+    #Remove the hyperlinks and make it numeric - Year it opened
+    stadium_data['Opened'] = stadium_data['Opened'].str.replace(r'\[.*?\]', '', regex=True)
+    stadium_data['Opened'] = pd.to_numeric(stadium_data['Opened'])
+
+    #Age of stadium is dependent on the current year subtracted by when it was opened
+    current_year = datetime.now().year
+    stadium_data['Age'] = current_year - stadium_data['Opened'] 
+    
+    #Team name
+    stadium_data['Team'] = stadium_data['Team'].str.replace(r'\[.*?\]', '', regex=True)
 
     #Want to get city and state by their seperate columns 
-    stadium_data[["City", "State"]] = stadium_data["Location"].str.split(", ", expand=True)
+    stadium_data[['City', 'State']] = stadium_data['Location'].str.split(', ', expand=True)
 
     #Then remove redundant location column
     stadium_data = stadium_data.drop(['Location'], axis = 1)
 
+
     return(stadium_data)
 
-clean_stadium_data(a)
