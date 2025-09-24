13efe97643fe49ad6c105eee9902a9e337cb052e
pakdamie <damiedpak@gmail.com>
2025-09-23 21:26:20 -0400

    Feat:New code for wrangling scraped hotdog price

--- src/wrangle_data.py
+++ src/wrangle_data.py
@@ -1,11 +1,13 @@
 import pandas as pd
 from datetime import datetime
 
-
 def clean_stadium_data(stadium_data):
 
     """
     Wrangles the stadium_data that was scraped from wikipedia
+
+    Args:
+        stadium_data (pd.DataFrame): Raw stadium data including capacity, team, and location.
 
     Returns:
         pd.DataFrame: A DataFrame containing the Stadium name, capacity, team that it hosts, when it opened,
@@ -16,9 +18,12 @@ def clean_stadium_data(stadium_data):
     stadium_data = stadium_data.drop(['Image', 'Surface', 'Distance to center field', 'Type', 'Roof type'], axis=1)
     
     #Remove the hyperlinks, remove the comma, and make it numeric - Capacity
-    stadium_data['Capacity'] = stadium_data['Capacity'].str.replace(r'\[\d+\]', '', regex=True)
-    stadium_data['Capacity'] = stadium_data['Capacity'].str.replace(',', '')
-    stadium_data['Capacity'] = pd.to_numeric(stadium_data['Capacity'])
+    stadium_data['Capacity'] = (
+    stadium_data['Capacity']
+    .str.replace(r'\[\d+\]', '', regex=True)
+    .str.replace(',', '', regex=False)
+    .astype(int)
+    )
 
     #Remove the hyperlinks and make it numeric - Year it opened
     stadium_data['Opened'] = stadium_data['Opened'].str.replace(r'\[.*?\]', '', regex=True)
@@ -37,6 +42,18 @@ def clean_stadium_data(stadium_data):
     #Then remove redundant location column
     stadium_data = stadium_data.drop(['Location'], axis = 1)
 
+    #Shortened team name
+    stadium_data['Team_Short']  = stadium_data['Team'].str.split().str[-1]
 
     return(stadium_data)
 
+
+
+def clean_hotdog_data(hotdog_data):
+    #Turn D-Backs into Diamondbacks
+    hotdog_data["Team"] = hotdog_data["Team"].replace("D-backs", "Diamondbacks")
+    
+    #Turned A's into Atheltics
+    hotdog_data["Team"] = hotdog_data["Team"].replace("A's", "Athletics")
+
+    return(hotdog_data)
